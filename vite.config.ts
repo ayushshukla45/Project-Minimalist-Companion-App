330641144faffca471a34c9b052b447bc92bb42c
Ayush Shukla <urayushshukla@example.com>
2025-09-28 02:08:11 +0530

    Fix vite base for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Project-Minimalist-Companion-App/',
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+})

71c6de5b89002ac67491780eed667d71bcaa9355
Ayush Shukla <urayushshukla@example.com>
2025-10-06 15:01:56 +0530

    Fix: set Vite base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   base: '/Project-Minimalist-Companion-App/',
   plugins: [react()],
-})
+});
